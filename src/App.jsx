ab97b4441746fcf092595d4ed21594014c2fd5aa
agorbacheva-y <agorbacheva.y@gmail.com>
2023-11-01 22:59:41 +0100

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 // Import required  hooks
+import { useState, useEffect } from "react";
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+  const [dogFact, setDogFact] = useState(null);
+  
   // Hint: Define the API endpoint
+  const url = "https://dogapi.dog/api/v2/facts";
+
   // Hint: Create a function to fetch the dog fact
+  const fetchDogFact = async () => {
+    // using .fetch to make request to url
+    await fetch(url)
+      // chain .then to handle response, convert to json format
+      .then((res) => res.json())
+      // handle json data
+      .then((data) => {
+        // check if data.data exists
+        if (data.data) {
+          // store data.data in variable cleanData
+          let cleanData = data.data;
+          console.log(cleanData);
+          console.log(cleanData[0].attributes);
+          // update dogFact state with body attribute of first item in cleanData
+          setDogFact(cleanData[0].attributes.body);
+        }
+      })
+      // chain .catch to handle errors
+      .catch((err) => {
+        console.log("Error reading dog fact:", err);
+      });
+  };
+
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  useEffect(() => {
+    fetchDogFact();
+  },[]);
 
   return (
     <div className="App">
-      <DogFact />
+      <DogFact dogFact={dogFact} />
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,10 +1,10 @@
 // DogFact Component
-export const DogFact = () => {
+export const DogFact = ({ dogFact }) => {
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop. We'll call this 'cleanFact'.
   // Cleaning our data means choosing and keeping a somewhat small fraction of the data, the most useful data.
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return <div>{dogFact ? dogFact : "loading..."}</div>;
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
